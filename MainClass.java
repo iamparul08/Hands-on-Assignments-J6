1d2e5a5aa526eddf5f6eb58032df9e47a231ffec
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-09 12:44:58 +0530

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,9 +1,3 @@
-/*Create a class named Animal which includes methods like eat() and sleep().
-Create a child class of Animal named Bird and override the parent class methods.
-Add a new method named fly().
-Create an instance of Animal class and invoke eat and sleep methods using this object.
-Create an instance of Bird class and invoke eat, sleep and fly methods using this object.*/
-
 class Animal {
 		
 		public void eat() {
